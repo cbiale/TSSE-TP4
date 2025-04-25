89d749b982248230d40c672c8bf2220788cb8a0f
cbiale <claudio_biale@yahoo.com.ar>
2025-04-25 00:10:32 -0300

    cambios

--- build/test/runners/test_leds_runner.c
+++ build/test/runners/test_leds_runner.c
@@ -10,7 +10,7 @@
 
 int GlobalExpectCount;
 int GlobalVerifyOrder;
-char* GlobalOrderError;
+char *GlobalOrderError;
 
 /*=======External Functions This Runner Calls=====*/
 extern void setUp(void);
@@ -27,28 +27,21 @@ extern void test_revisar_limites_inferior(void);
 extern void test_revisar_limites_superior(void);
 extern void test_revisar_parametros_fuera_de_limites(void);
 
-
 /*=======Mock Management=====*/
-static void CMock_Init(void)
-{
+static void CMock_Init(void) {
   GlobalExpectCount = 0;
   GlobalVerifyOrder = 0;
   GlobalOrderError = NULL;
 }
-static void CMock_Verify(void)
-{
-}
-static void CMock_Destroy(void)
-{
-}
+static void CMock_Verify(void) {}
+static void CMock_Destroy(void) {}
 
 /*=======Teardown (stub)=====*/
 void tearDown(void) {}
 
 /*=======Test Reset Options=====*/
 void resetTest(void);
-void resetTest(void)
-{
+void resetTest(void) {
   tearDown();
   CMock_Verify();
   CMock_Destroy();
@@ -56,48 +49,40 @@ void resetTest(void)
   setUp();
 }
 void verifyTest(void);
-void verifyTest(void)
-{
-  CMock_Verify();
-}
+void verifyTest(void) { CMock_Verify(); }
 
 /*=======Test Runner Used To Run Each Test=====*/
-static void run_test(UnityTestFunction func, const char* name, UNITY_LINE_TYPE line_num)
-{
-    Unity.CurrentTestName = name;
-    Unity.CurrentTestLineNumber = (UNITY_UINT) line_num;
+static void run_test(UnityTestFunction func, const char *name,
+                     UNITY_LINE_TYPE line_num) {
+  Unity.CurrentTestName = name;
+  Unity.CurrentTestLineNumber = (UNITY_UINT)line_num;
 #ifdef UNITY_USE_COMMAND_LINE_ARGS
-    if (!UnityTestMatches())
-        return;
+  if (!UnityTestMatches())
+    return;
 #endif
-    Unity.NumberOfTests++;
-    UNITY_CLR_DETAILS();
-    UNITY_EXEC_TIME_START();
-    CMock_Init();
-    if (TEST_PROTECT())
-    {
-        setUp();
-        func();
-    }
-    if (TEST_PROTECT())
-    {
-        tearDown();
-        CMock_Verify();
-    }
-    CMock_Destroy();
-    UNITY_EXEC_TIME_STOP();
-    UnityConcludeTest();
+  Unity.NumberOfTests++;
+  UNITY_CLR_DETAILS();
+  UNITY_EXEC_TIME_START();
+  CMock_Init();
+  if (TEST_PROTECT()) {
+    setUp();
+    func();
+  }
+  if (TEST_PROTECT()) {
+    tearDown();
+    CMock_Verify();
+  }
+  CMock_Destroy();
+  UNITY_EXEC_TIME_STOP();
+  UnityConcludeTest();
 }
 
 /*=======MAIN=====*/
- int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
 #ifdef UNITY_USE_COMMAND_LINE_ARGS
   int parse_status = UnityParseOptions(argc, argv);
-  if (parse_status != 0)
-  {
-    if (parse_status < 0)
-    {
+  if (parse_status != 0) {
+    if (parse_status < 0) {
       UnityPrint("test_leds.");
       UNITY_PRINT_EOL();
       UnityPrint("  test_todos_los_leds_inician_apagados");
@@ -128,17 +113,22 @@ static void run_test(UnityTestFunction func, const char* name, UNITY_LINE_TYPE l
   }
 #endif
   UnityBegin("test_leds.c");
-  run_test(test_todos_los_leds_inician_apagados, "test_todos_los_leds_inician_apagados", 77);
+  run_test(test_todos_los_leds_inician_apagados,
+           "test_todos_los_leds_inician_apagados", 77);
   run_test(test_prender_led_individual, "test_prender_led_individual", 85);
   run_test(test_apagar_led_individual, "test_apagar_led_individual", 91);
-  run_test(test_prender_y_apagar_algunos_leds, "test_prender_y_apagar_algunos_leds", 98);
+  run_test(test_prender_y_apagar_algunos_leds,
+           "test_prender_y_apagar_algunos_leds", 98);
   run_test(test_prender_todos_los_leds, "test_prender_todos_los_leds", 107);
   run_test(test_apagar_todos_los_leds, "test_apagar_todos_los_leds", 113);
-  run_test(test_consultar_estado_led_encendido, "test_consultar_estado_led_encendido", 120);
-  run_test(test_consultar_estado_led_apagado, "test_consultar_estado_led_apagado", 126);
+  run_test(test_consultar_estado_led_encendido,
+           "test_consultar_estado_led_encendido", 120);
+  run_test(test_consultar_estado_led_apagado,
+           "test_consultar_estado_led_apagado", 126);
   run_test(test_revisar_limites_inferior, "test_revisar_limites_inferior", 132);
   run_test(test_revisar_limites_superior, "test_revisar_limites_superior", 138);
-  run_test(test_revisar_parametros_fuera_de_limites, "test_revisar_parametros_fuera_de_limites", 143);
+  run_test(test_revisar_parametros_fuera_de_limites,
+           "test_revisar_parametros_fuera_de_limites", 143);
 
   return UNITY_END();
 }
